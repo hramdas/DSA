491283c57aa3562d221a5c489b4be63ee85ab0c5
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-25 22:42:59 +0530

    Super digit

---
+++ recursion/TheFamousSum.js
@@ -0,0 +1,44 @@
+function runProgram(input) {
+    input = input.trim().split(' ')
+    var n = input[0].trim()
+    var k = +input[1].trim()
+
+    if(n.length > 1){
+        //number to string and arr of nuumber
+        n = n.toString().split('').map(Number)
+        n = n.reduce((a,b)=>a+b, 0)  //sum of all digits
+        n = n*k
+    }
+
+    console.log(superDigit(n))
+}
+function superDigit(n){
+    if(n < 10) return n
+
+    n = n.toString().split('').map(Number)   
+    digit = n.reduce((a,b)=>a+b, 0)  //sum of all digits
+
+    return superDigit(digit)
+}
+  
+if (process.env.USERNAME === "hedga") {
+    runProgram(`123 3`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+}
+  
