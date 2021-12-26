5d0a45cad3a4cad7a46091679401c94a18cd0813
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-26 23:14:56 +0530

    Recursion

--- recursion/Permutations.js
+++ recursion/Permutations.js
@@ -1,11 +1,36 @@
+function permutation(arr, l, n){
+    if(l==n){
+        console.log(arr.join(' '))
+        return
+    }
+    
+    for(let i=l; i<=n; i++){
+       swap(arr, i, l)
+        permutation(arr, l+1, n)
+        swap(arr, i, l)
+    }
+}
+
+///1 2 3
+//
+
+function swap(arr, i, j){
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    return arr
+}    
+
 function runProgram(input) {
-    input = input.trim().split('')
+    input = input.trim().split('\n')
     var n = +input[0].trim()
-   
+    var arr = input[1].trim().split(' ').map(Number)
+   permutation(arr, 0, n-1)
 }
   
 if (process.env.USERNAME === "hedga") {
-    runProgram(``);
+    runProgram(`3
+    1 2 3`);
   } else {
     process.stdin.resume();
     process.stdin.setEncoding("ascii");
