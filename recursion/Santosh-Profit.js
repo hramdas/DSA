ae71e62f1ac9aebde2d3a16a354555284b8c5d08
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-26 10:26:35 +0530

    Max profit possibility

---
+++ recursion/Santosh-Profit.js
@@ -0,0 +1,42 @@
+function runProgram(input) {
+    input = input.trim().split('\n').map(Number)
+    var cases = input[0]
+   for(let i=1; i<=cases; i++){
+       var n = input[i]
+      console.log(profit(n))
+   }
+}
+
+function profit(n){
+  if(n == 0) return 1
+  if(n < 0) return 0
+  return profit(n-4) + profit(n-8)
+
+         //12
+  // 8            //4
+  //4  //0      //0   //-4
+  //0
+}
+  
+if (process.env.USERNAME === "hedga") {
+    runProgram(`1
+    12`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+}
+  
