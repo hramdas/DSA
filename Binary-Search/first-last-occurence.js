d7e23ae87a6d6b4903a3793465a50f9d6e4eaccc
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-07 13:10:13 +0530

    first and last occurance

---
+++ Binary-Search/first-last-occurence.js
@@ -0,0 +1,31 @@
+function runProgram(input) {
+    input = input.trim().split('\n')
+    var n = +input[0].trim()
+    var k = +input[2].trim()
+    var arr= input[1].trim().split(' ').map(Number)
+    console.log(arr, k)
+}
+  
+if (process.env.USERNAME === "hedga") {
+    runProgram(`6
+    1 1 1 2 2 2	
+    1`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+}
+  
