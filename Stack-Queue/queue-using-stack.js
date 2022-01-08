b35a5d338eacc15c0ff8369ebd1db72dfb8f9a28
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-08 10:03:21 +0530

    initial

--- Stack-Queue/queue-using-stack.js
+++ Stack-Queue/queue-using-stack.js
@@ -3,7 +3,27 @@
 function runProgram(input) {
     input = input.trim().split('\n')
     var n = +input[0].trim()
-   
+     s1 = []
+     s2 = []
+    for(var i=1 ;i<=n; i++){
+      var line = input[i].trim().split(' ').map(Number)
+      if(line[0] === 0) s1.push(line[1])
+      if(line[0] === 2)  deQueue(s1, s2)
+      console.log('i', i, line[1], s1)
+    }
+}
+
+function deQueue(s1, s2){
+  
+    while(s1.length !== 0 ){
+      s2.push(s1.pop())
+    }
+    console.log(s2.pop())
+
+    while(s2.length !== 0){
+      s1.push(s2.pop)
+    }
+    return
 }
   
 if (process.env.USERNAME === "hedga") {
