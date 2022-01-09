7a9f23a68e1d8f748e07a5f90b1c47e7d6da14ea
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-09 23:57:51 +0530

    binary

--- Binary-Search/first-last-occurence.js
+++ Binary-Search/first-last-occurence.js
@@ -3,7 +3,44 @@ function runProgram(input) {
     var n = +input[0].trim()
     var k = +input[2].trim()
     var arr= input[1].trim().split(' ').map(Number)
-    console.log(arr, k)
+    console.log(occurance(arr, k, n))
+}
+function  occurance(arr, k, n){
+  var i = lower(arr, k, 0, n)
+  if(i !== -1){
+    var j = upper(arr, k, i, n)
+  }
+  console.log(i, j)
+  // return (j-i+1)
+  //else return (-1, -1, 0)
+}
+
+function lower(arr, k, l, h){
+  let res = -1
+  while(h>=l){
+    let mid = Math.floor(l + (h-l)/2)
+
+    if(arr[mid] == k){
+      res = mid;
+      h=mid-1
+    } else if(k>arr[mid]){
+      l = mid+1
+    } else h = mid-1
+  }
+  return res
+}
+
+function upper(arr, k, l, h){
+  while(h>=l){
+    let mid = Math.floor(l+(h-1)/2)
+    if(arr[mid] == k){
+      res = mid;
+      l = mid+1
+    } else if(k>arr[mid]){
+      l = mid+1
+    } else h=mid-1
+  }
+  return res
 }
   
 if (process.env.USERNAME === "hedga") {
