db84e3cf9cfdec2992222dc2d2c36ef6fa824c35
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-02-24 12:41:01 +0530

    sub array

--- Arrays/subArray.js
+++ Arrays/subArray.js
@@ -1,37 +1,33 @@
-
-var a =  [1, 2, 3, 4]
-function subArr(a){
-    for(let i=0; i<a.length; i++){
-        var out = []
-        for(let j=i; j<a.length; j++){
-            out.push(a[j])
-            //res.push(out)
-           console.log(out)
-           //console.log(i, 'res', res)
-        }
+var a = [1, 2, 3, 4];
+function subArr(a) {
+  for (let i = 0; i < a.length; i++) {
+    var out = [];
+    for (let j = i; j < a.length; j++) {
+      out.push(a[j]);
+      //res.push(out)
+      console.log(out);
+      //console.log(i, 'res', res)
     }
+  }
 }
-console.log( subArr(a))
+// subArr(a);
 
+// ----------------------------------------------------- //
 
-
-// let arr = [1, 2, 3, 4]
-let arr = "ABCD"
-function subArray(n){
-    let col = []
-    for(let i = 0; i < n; i++)
-    {
-        for(let j = i; j < n; j++)
-        {
-            let subseq = []
-            for(let k = i; k <= j; k++){
-               subseq.push(arr[k])
-            }
-            col.push(subseq)
-        }
+let arr = [1, 2, 3, 4];
+// let arr = "ABCD";
+function subArray(n) {
+  let col = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      let subseq = [];
+      for (let k = i; k <= j; k++) {
+        subseq.push(arr[k]);
+      }
+      col.push(subseq);
     }
-    return col
+  }
+  return col;
 }
 
-// console.log(subArray(arr.length))
-
+console.log(subArray(arr.length));
