fda70323178b7b8c227d4c2cfdf5eed318998ae8
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-03-02 22:39:41 +0530

    radio substance remaining

--- radio-active.js
+++ radio-active.js
@@ -1,7 +1,8 @@
-// Take a radio active substance add some amount in it daily in how many it will be zero
+// Take a radio active substance add some amount in it daily
+//Calculate remaining amount after  given daya
 let x = 5; //gm;
 let y = 1; //gm;
-let n = 5;
+let n = 5; //days
 let c = 0;
 console.log(remaining(x, c));
 function remaining(x, n) {
