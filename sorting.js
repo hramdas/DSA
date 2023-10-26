98f67217007e04b9a515b6a60957d85da7105842
Ramdas <ramdas.h@tricog.com>
2023-10-26 11:18:51 +0530

    quick sort

--- sorting.js
+++ sorting.js
@@ -35,4 +35,22 @@ function merge(left, right) {
   return [...sortedArray, ...left, ...right]
 }
 
-console.log("Merge Sort :", mergeSort(arr))
+console.log("Merge Sort  :", mergeSort(arr))
+
+
+
+function quickSort(arr) {
+  if (arr.length <= 1) return arr
+
+  const pivot = arr[0]
+  const left = []
+  const right = []
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i])
+    } else right.push(arr[i])
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+console.log("Quick Sort  :", quickSort(arr))
